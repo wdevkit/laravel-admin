d7b7ed1fcad7fad9caa89acedabaaf1ea115695b
Gilmar <wdarking@gmail.com>
2020-09-10 23:35:58 -0300

    Add admin hasFactory trait

--- src/Models/Admin.php
+++ src/Models/Admin.php
@@ -2,13 +2,14 @@
 
 namespace Wdevkit\Admin\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
