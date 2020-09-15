eb8ab9d53668dbcba7865ec528ebac266b1769db
Gilmar <wdarking@gmail.com>
2020-09-15 17:56:37 -0300

    Set name on login post route

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::name('wdevkit_admin.')->group(function () {
 
     Route::get('login', [\Wdevkit\Admin\Http\Controllers\LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('login', [\Wdevkit\Admin\Http\Controllers\LoginController::class, 'login']);
+    Route::post('login', [\Wdevkit\Admin\Http\Controllers\LoginController::class, 'login'])->name('login_post');
 
     Route::middleware('auth:web_admin')->group(function () {
         Route::get('home', \Wdevkit\Admin\Http\Controllers\HomeController::class)->name('home');
